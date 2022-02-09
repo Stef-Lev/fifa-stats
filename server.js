224fc45f8b1391b4351fdea78abcee90c24e87c4
Stefanos <sigianas@yahoo.com>
2022-02-09 23:25:24 +0200

    fix login

--- server.js
+++ server.js
@@ -71,8 +71,10 @@ app.get('/api/games/:id', game.getOne);
 app.delete('/api/tournaments/:tid/game/:gid', game.delete);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
-  app.use('*', express.static(path.join(__dirname, "client", "build")))
+  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
 } else {
   app.get('/', (req, res) => {
     res.send('API running');
