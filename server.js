5c038236c605004eb34e595ac9bf012370a3506f
Stefanos <sigianas@yahoo.com>
2022-02-10 21:24:38 +0200

    fix blank page

--- server.js
+++ server.js
@@ -71,9 +71,10 @@ app.get('/api/games/:id', game.getOne);
 app.delete('/api/tournaments/:tid/game/:gid', game.delete);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  const root = require('path').join(__dirname, 'client', 'build');
+  app.use(express.static(root));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile('index.html', { root });
   });
 } else {
   app.get('/', (req, res) => {
